03f91f5d2a04f5fe1db6d660fe235d8e055b51b4
Aaron <aaronbaw@gmail.com>
2016-05-30 23:28:48 +0200

    improvements to responsive design
    navbar now in fixed pos, corrected issue with unclickable mobnav
    
    content position adjustments to #about to make it more responsive and
    tidier
    
    addition of scrollTo jquery functions to scroll on click

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,6 +1,29 @@
 $( document ).ready(function() {
+//hamburger menu
   $('button').click(function(){
     $(this).toggleClass('is-active')
+  });
+
+//NavigationDesktop
+function scrollToAnchor(aid){
+    var aTag = $("a[name='"+ aid +"']");
+    $('html,body').animate({scrollTop: aTag.offset().top},'slow');
+}
+
+
+  $('#about').click(function(){
+    scrollToAnchor('about')
+    return false
+  })
+
+  $('.downNavArrow').click(function(){
+    scrollToAnchor('about')
+    return false
+  })
+
+  $('#home').click(function(){
+    scrollToAnchor('home')
+    return false
   })
 
 });
