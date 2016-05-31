ce7a6e7643a0d33235565e415699e219a0cda8aa
Aaron <aaronbaw@gmail.com>
2016-05-31 16:20:46 +0200

    navbar fade and other stuff

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -4,6 +4,26 @@ $( document ).ready(function() {
     $(this).toggleClass('is-active')
   });
 
+//Opaque Navigation toggle on scroll
+
+  //finding the position from top of the anchor point
+
+
+
+$(window).scroll(function(){
+var $scrollDistance = $(window).scrollTop();
+var $anchorPos = $("a[name=about]").offset().top;
+
+if ($scrollDistance >= $anchorPos) {
+  $('.nwrap').addClass('opaqueNav')
+  $('nav').addClass('darkerNavItems')
+} else {
+  $('.nwrap').removeClass('opaqueNav')
+  $('nav').removeClass('darkerNavItems')
+}
+
+})
+
 //NavigationDesktop
 function scrollToAnchor(aid){
     var aTag = $("a[name='"+ aid +"']");
