96a9a89f474da48899bf1066fce71345962b68f7
Aaron <aaronbaw@gmail.com>
2016-06-01 23:08:56 +0200

    smoother scroll with velocityJS & beginning work to hamburger menu mobile.

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,49 +1,49 @@
 $( document ).ready(function() {
-//hamburger menu
+//hamburger menu [MOBILE]
   $('button').click(function(){
     $(this).toggleClass('is-active')
+    $('.body').toggleClass('hamTrans')
   });
 
 //Opaque Navigation toggle on scroll
 
   //finding the position from top of the anchor point
-
-
-
 $(window).scroll(function(){
 var $scrollDistance = $(window).scrollTop();
-var $anchorPos = $("a[name=about]").offset().top;
-
-if ($scrollDistance >= $anchorPos) {
+var $anchorPos = {
+  about: $("a[name=about]").offset().top,
+  home: $("a[name=home]").offset().top
+}
+  //setting the opaque nav classes:
+if ($scrollDistance >= $anchorPos.about) {
   $('.nwrap').addClass('opaqueNav')
   $('nav').addClass('darkerNavItems')
 } else {
   $('.nwrap').removeClass('opaqueNav')
   $('nav').removeClass('darkerNavItems')
 }
-
 })
 
 //NavigationDesktop
-function scrollToAnchor(aid){
-    var aTag = $("a[name='"+ aid +"']");
-    $('html,body').animate({scrollTop: aTag.offset().top},'slow');
-}
 
+$('a[href*=#]').on('click', function(e){
+    e.stopPropagation()
+    e.preventDefault()
+    var href = $(this).attr('href');
+    var dest = 'a[name=' + href.replace('#','') + ']';
+    $(dest).velocity('scroll',{
+      duration: 'slow',
+      ease: 'ease-in-out'
+    })
+});
 
-  $('#about').click(function(){
-    scrollToAnchor('about')
-    return false
+$('.downNavArrow').on('click',function(e){
+  $('a[name=about]').velocity('scroll',{
+    duration: 'slow',
+    ease: 'ease-in-out'
   })
+})
 
-  $('.downNavArrow').click(function(){
-    scrollToAnchor('about')
-    return false
-  })
 
-  $('#home').click(function(){
-    scrollToAnchor('home')
-    return false
-  })
 
 });
