33a40c6900e1ba6a991cd492aea10c0077147509
Aaron <aaronbaw@gmail.com>
2016-06-10 11:47:47 +0200

    fixed tweet content

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,10 @@
 $( document ).ready(function() {
+//removing media from tweetembed
+  $('.twitterFeed').on('DOMSubtreeModified propertychange','#twitter-widget-0', function(){
+    $('.twitter-timeline').contents().find('.timeline-Tweet-media').css('display','none')
+    $('.twitter-block').css('height','100%')
 
+  });
 //deferred image load
 
 function mobMenuSlide(){
