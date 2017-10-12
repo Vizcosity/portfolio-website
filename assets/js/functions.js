600ba83f10a2558dc92893f1fa7066458c0728de
Aaron Baw <aaronbaw@Aarons-MacBook-Pro.local>
2017-10-12 14:54:57 +0100

    implement dynamic about section, tweak page link colours

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -3,10 +3,6 @@ const accessToken = 'e0588852d2401c2c2b331a719151d9f8feae5aca98cadfe968be9183c42
 
 var categories = [];
 
-var globalAttributes = {};
-
-var i = 0;
-
 $(document).ready(function(){
 
   // Entry point.
@@ -17,9 +13,6 @@ $(document).ready(function(){
 			typeSpeed: 75,
       loop: true
 		});
-
-  // Portfolio item content grid layout with jQuery MixItUp 3.
-  var grid = mixitup(".items-container");
 
   // Scroll revealing.
   window.sr = ScrollReveal();
@@ -39,8 +32,13 @@ $(document).ready(function(){
 
   });
 
+  // Load the about section.
+  loadAboutSection(spaceID, accessToken);
+
   // Portfolio Item expand / collapse functionality.
   $('.items-container').on('click', '.expandable', function(){
+
+    // $(this).css('display: none');
 
     expandPortfolioItem($(this), ($expanded) => {
 
@@ -139,8 +137,6 @@ function revealPortfolioContent($element, callback){
 // Loads a portfolio article and grabs renders to the element.
 function loadPortfolioArticle($element, callback){
 
-
-
   // Portfolio item id.
   var id = $element.data('id');
 
@@ -160,6 +156,14 @@ function loadPortfolioArticle($element, callback){
 
 }
 
+// Parses md to HTML.
+function parseMD(md){
+
+  var parseMarkdown = new showdown.Converter();
+  return parseMarkdown.makeHtml(md);
+
+}
+
 // Article object.
 function Article(md){
 
@@ -176,6 +180,7 @@ function Article(md){
 
 // Expands a passed portfolio item.
 function expandPortfolioItem($element, callback){
+
   // Duplicate the current element, and inject into page as a fixed pos overlay.
     // We first create a jQuery element object and prepare it to be injected into the page.
     // var overlayElement = $('<div class="portfolio-item">'+$(this).html()+'</div>');
@@ -184,8 +189,14 @@ function expandPortfolioItem($element, callback){
     // Save the position + size attributes of the item as it is on the page.
     var elementAttributes = $element[0].getBoundingClientRect();
 
+    // Remove the unsupported 'x' and 'y' properties.
+    elementAttributes = cleanExpansionElementAttributes(elementAttributes);
+
     // Save the attributes in the global variable, to be used later when collapsing.
     // globalAttributes[$(this).data("id")] = elementAttributes;
+    //
+    console.log(`Incoming collapse stuff.`);
+    console.log(elementAttributes);
 
     prepareCollapseAnimation(elementAttributes, $element);
 
@@ -238,7 +249,18 @@ function expandPortfolioItem($element, callback){
 
 
         // Inject the exit button.
-        $innerContent.append('<button class="portfolio-item-extended-exit-button"> <svg style="display: none;" stroke="#000000" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><line stroke-width="4" x1="2" x2="30" y1="2" y2="30"/><line stroke-width="4" x1="2" x2="30" y1="30" y2="2"/></svg></button>');
+        $innerContent.append(`
+          <button class="portfolio-item-extended-exit-button tooltipped">
+          <!-- SVG ICON -->
+            <svg style="display: none;" stroke="#000000" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+              <line stroke-width="4" x1="2" x2="30" y1="2" y2="30"/><line stroke-width="4" x1="2" x2="30" y1="30" y2="2"/>
+            </svg>
+
+          <!-- TOOLTIP -->
+            <p class='tooltip-content'>
+              Back.
+            </p>
+          </button>`);
 
         // After appending the exit button, allow for a soft fadeIn.
         $("button.portfolio-item-extended-exit-button > svg").fadeIn();
@@ -275,6 +297,46 @@ function PortfolioItem(details, id){
   this.attributes = details;
 }
 
+// Fetches the about section and loads it into the page.
+function loadAboutSection(spaceID, accessToken){
+
+  // Get the JSON info from contentful.
+  getAboutSection(spaceID, accessToken, (details) => {
+
+    render({markup: details.title}, $('h2#about-section-header'));
+
+    // Parse the markdown from contentful.
+
+
+    render({markup: parseMD(details.body)}, $('p.about-body'));
+
+  });
+
+}
+
+// Fetch the about section from contentful.
+function getAboutSection(spaceID, accessToken, callback){
+
+  // Instance contentful client.
+  var client = contentful.createClient({
+    space: spaceID,
+    accessToken: accessToken
+  });
+
+  // Fetch about section.
+  client.getEntries({
+    'content_type': 'about'
+  })
+  // Return the first entry.
+  .then(response => {
+    return callback({
+      title: response.items[0].fields.sectionHeader,
+      body: response.items[0].fields.sectionBody
+    });
+  })
+
+}
+
 // Grabbing the portfolio items from Contentful:
 function fetchItemsJSON(spaceID, accessToken, callback){
 
@@ -284,7 +346,9 @@ function fetchItemsJSON(spaceID, accessToken, callback){
   });
 
   // Collect the portfolio item entries in raw JSON:
-  client.getEntries().then(function(entries){
+  client.getEntries({
+    content_type: 'portfolioEntry'
+  }).then(function(entries){
 
     var output = [];
     for (var i = 0; i < entries.items.length; i++){
@@ -338,9 +402,12 @@ function render(element, container){
   $(container).append(element.markup);
 }
 
+// Stylesheet incrementer.
+var i = 0;
+
 // Prepares the collapse animation by adding the positions and sizes to the stylesheet.
 // Dynamic aniamtion.
-function prepareCollapseAnimation(attributes, element){
+function prepareCollapseAnimation(attributes, $element){
 
   var styles = '';
 	styles = '@keyframes collapseItem {';
@@ -350,10 +417,10 @@ function prepareCollapseAnimation(attributes, element){
 		styles += 'top: 0px;';
 		styles += 'left: 0px;';
 		styles += '}';
-		styles += '50% {';
-		styles += 'height: '+attributes.height+'px;';
-		styles += 'top: ' + attributes.top + 'px;';
-		styles += '}';
+		// styles += '50% {';
+		// styles += 'height: '+attributes.height+'px;';
+		// styles += 'top: ' + attributes.top + 'px;';
+		// styles += '}';
 		styles += '100% {';
 		styles += 'height: '+attributes.height+'px;';
 		styles += 'width: '+attributes.width+'px;';
@@ -363,5 +430,27 @@ function prepareCollapseAnimation(attributes, element){
 	styles += '}';
 
   document.styleSheets[0].insertRule(styles, i++);
+
+}
+
+// Cleans the 'x' and 'y' properties which are not supported in CSS.
+function cleanExpansionElementAttributes($element){
+
+  // the DOMRect object seems to not produce any keys when
+  // using the Object.keys() function, therefore we have to map
+  // all of our desired attribtes manually..
+
+  var $output = {
+    width: $element.width,
+    height: $element.height,
+    bottom: $element.bottom,
+    left: $element.left,
+    right: $element.right,
+    top: $element.top,
+
+  };
+
+  // Return the cleaned element.
+  return $output;
 
 }
